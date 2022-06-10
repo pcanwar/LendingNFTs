c59a41ed3f5a85dc5ebae650f2adaff916736614
pcanwar <aalruwai@stevens.edu>
2022-06-10 14:33:07 -0400

    lending merkle time

--- contracts/MerkleLendingv2.sol
+++ contracts/MerkleLendingv2.sol
@@ -13,12 +13,13 @@ Counters.Counter private _tokenIdCounter;
 
 struct Root {
     bytes32 root;
-    uint256 paymentLenigh;
+    address brower;
 
 }
 mapping (uint256 => Root) private _root;
 
-mapping (address => bool) private isMinted;
+mapping (uint256 => mapping (uint256 => bool)) private isPaid;
+mapping (uint256 => uint256) private counterPayment;
 
 constructor(string memory name, string memory symbol)
 ERC721(name, symbol)
@@ -26,23 +27,14 @@ ERC721(name, symbol)
  
 }
 
-function setRoot(bytes32 root_, uint256 paymentLenght_) public {
+function setRoot(bytes32 root_) public {
     uint256 tokenId = _tokenIdCounter.current();
     _tokenIdCounter.increment();
-    _root[_tokenIdCounter].root = root_;
-    _root[_tokenIdCounter].paymentLenght = paymentLenght_;
+    _root[tokenId].root = root_;
+    _root[tokenId].brower = msg.sender;
 }
 
-function airdrop(address account, bytes32[] calldata proof) external
-{
-    require(account==msg.sender,"not owner" );
-    // you can have some requirement by checking how many mint per address
-    // or map address -> true/false something like isMinted:
-    require(!isMinted[account]);
-    require(_verify(_leaf(account), proof), "Invalid merkle proof");
-    isMinted[account] = true;
-    safeMint(account);
-}
+
 
 function safeMint(address to) private  {
     uint256 tokenId = _tokenIdCounter.current();
@@ -50,16 +42,29 @@ function safeMint(address to) private  {
     _safeMint(to, tokenId);
 }
 
-function _leaf(address account)
-public pure returns (bytes32)
-{
-    return keccak256(abi.encodePacked(account));
-}
+function redeem(address account, uint256 time_,  bytes32[] calldata proof)
+    external
+    {
+        require(counterPayment[1] == _tokenIdCounter.current());
+        require(time_ > block.timestamp);
+        // uint256 _IdCounter = _tokenIdCounter.current();
+        counterPayment[1] = _tokenIdCounter.current();
+        require(_verify(_leaf(account, time_),counterPayment[1], proof), "Invalid merkle proof");
+        require(account==msg.sender,"not owner" );
+        _tokenIdCounter.increment();
+        safeMint(account);
+    }
 
-function _verify(bytes32 leaf, bytes32[] memory proof)
-public view returns (bool)
-{
-    return MerkleProof.verify(proof, root, leaf);
-}
-}
+    function _leaf(address account, uint256 time_)
+    internal pure returns (bytes32)
+    {
+        return keccak256(abi.encodePacked(time_, account));
+    }
 
+    function _verify(bytes32 leaf, uint256 _IdCounter, bytes32[] memory proof)
+    internal view returns (bool)
+    {
+        bytes32 root = _root[_IdCounter].root;
+        return MerkleProof.verify(proof, root, leaf);
+    }
+}
